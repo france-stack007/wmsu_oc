ffdb4f6179a46f7f7546dd48184db568b155992a
france-stack007 <64594249+france-stack007@users.noreply.github.com>
2023-11-22 23:47:45 +0800

    Update system_settings.py

--- wmsu_oc/system_settings.py
+++ wmsu_oc/system_settings.py
@@ -1,6 +1,6 @@
-DB_HOST             = 'localhost'
+DB_HOST             = 'roundhouse.proxy.rlwy.net'
 DB_USERNAME         = 'root'
-DB_PASSWORD         = ''
-DB_PORT             = 3306
-DB_DATABASENAME     = 'wmsu_oc_db'
+DB_PASSWORD         = 'fF1hC1E-B43ec6GcdGh-HcGg5ehe6fgf'
+DB_PORT             = 15557
+DB_DATABASENAME     = 'railway'
 SECRET_KEY          = 'thesisDefended'
