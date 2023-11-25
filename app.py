ca96b072105125c06fe0f5b0456e3547730c3233
france-stack007 <64594249+france-stack007@users.noreply.github.com>
2023-11-25 13:27:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+from gunicorn.app.base import BaseApplication
 from wmsu_oc import create_app
 
 flask_app = create_app()
 
-if __name__ == "__main__":
-    # flask_app.run(debug=True)
-    flask_app.run(debug=True)
+class StandaloneApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+options = {
+    'bind': '0.0.0.0:5000',  # Adjust the host and port as needed
+    'workers': 4,  # Adjust the number of workers based on your needs
+    'timeout': 120  # Adjust the timeout if needed
+}
+
+StandaloneApplication(flask_app, options).run()
