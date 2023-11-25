660dcb0c15a6fed67fec909dc33a8ad577c31f77
france-stack007 <64594249+france-stack007@users.noreply.github.com>
2023-11-25 13:35:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from wmsu_oc import create_app
 
 flask_app = create_app()
 
-# if __name__ == "__main__":
-#     # flask_app.run(debug=True)
-#     flask_app.run(debug=True)
+if __name__ == "__main__":
+    # flask_app.run(debug=True)
+    flask_app.run(debug=True)
